537508242c2a7f519fbd200535649b133e9544ef
陳憶柔 <D1204387@o365.fcu.edu.tw>
2024-07-03 12:02:48 +0800

    新增程式

--- TemperatureConvert/app/src/main/java/com/example/temperatureconvert/MainActivity.java
+++ TemperatureConvert/app/src/main/java/com/example/temperatureconvert/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.temperatureconvert;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void convertToC(View view){
+
+        Button convertToFButton = (Button)findViewById(R.id.convertToF);
+        EditText temperatureEditView = (EditText)findViewById(R.id.temperature);
+
+        int temperature = Integer.parseInt(temperatureEditView.getText().toString());
+        int fTemperature = temperature * 9/ 5 + 32;
+
+
+
+    }
+
+    public void convertToF(View view){
+
     }
 }
