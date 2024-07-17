07366e54fbb85adf20384342d9ead4f77c25386b
陳憶柔 <D1204387@o365.fcu.edu.tw>
2024-07-17 16:30:35 +0800

    BankAccount修改

--- BankAccount/app/src/main/java/com/example/bankaccount/MainActivity.java
+++ BankAccount/app/src/main/java/com/example/bankaccount/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.bankaccount;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private double total;
+    private ActivityResultLauncher<Intent> intentActivityResultLanucher;
+    private TextView value;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,35 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
+        intentActivityResultLanucher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+                        // 這裡寫另一個Activity回傳後，獲取資料並處理的地方
+                        if(o.getData() != null && o.getResultCode() == Activity.RESULT_OK){
+                            bmi = o.getData().getDoubleExtra("BMI", -1);
+                            updateUI();
+                        }
+                    }
+                }
+        );
+    }
+
+    public void deposit(View View){
+
+        EditText amt = (EditText) findViewById(R.id.amount);
+
+        if
+
+
+
+    }
+
+    public void withdraw(View View){
+
     }
 }
