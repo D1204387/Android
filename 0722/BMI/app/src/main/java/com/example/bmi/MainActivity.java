4d42b2e60fd16e71f7ab90e6fabf0dc0c686d3e3
陳憶柔 <D1204387@o365.fcu.edu.tw>
2024-07-29 14:18:19 +0800

    新增0722資料夾
    Home Study

---
+++ 0722/BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.bmi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private double bmi;
+    private ActivityResultLauncher<Intent> intentActivityResultLauncher;
+    private TextView value;
+    private TextView description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        intentActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+
+                        if(o.getData() != null &&)
+
+                    }
+                }
+        );
+
+    }
+
+    public void GotoCalculateBMI(View view){
+
+        Intent intent = new Intent(this, CalculateBMIActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void updateUI(){
+        TextView result = (TextView) findViewById(R.id.Result);
+        result.setText(String.valueOf(bmi));
+    }
+}
