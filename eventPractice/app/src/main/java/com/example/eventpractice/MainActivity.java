98b8556529981bda9a6d2c39500d27f6a5f02a5f
陳憶柔 <D1204387@o365.fcu.edu.tw>
2024-07-31 12:04:09 +0800

    新增OnToch功能

--- eventPractice/app/src/main/java/com/example/eventpractice/MainActivity.java
+++ eventPractice/app/src/main/java/com/example/eventpractice/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return true;
     }
 
-    @Override
+
     public boolean onTouch(View view, MotionEvent motionEvent) {
 
 
